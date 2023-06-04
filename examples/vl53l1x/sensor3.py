e9d542b5d8eb61320daffcfeb44950e746020382
plutonh <jonghyen51@gmail.com>
2023-06-04 22:55:34 +0900

    Fix sensor3.py code

--- examples/vl53l1x/sensor3.py
+++ examples/vl53l1x/sensor3.py
@@ -27,15 +27,11 @@ print("""
 Press Ctrl+C to exit
 """)
 
-tof_1 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
-tof_1.open()
-
-tof_1.start_ranging(1) # 1 means short range
-
-tof_2 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
-tof_2.open()
-
-tof_2.start_ranging(1) # 1 means short range
+tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+tof.open()
+GPIO.output(sensor_1_shutdown, GPIO.HIGH)
+GPIO.output(sensor_2_shutdown, GPIO.HIGH)
+tof.start_ranging(1) # 1 means short range
 
 select = 0
 count = 0
@@ -44,47 +40,40 @@ limit = 1000
 
 running = True
 
-def exit_handler_1(signal, frame):
+def exit_handler(signal, frame):
     global running
     running = False
-    tof_1.stop_ranging()
+    tof.stop_ranging()
     print()
     sys.exit(0)
 
-def exit_handler_2(signal, frame):
-    global running
-    running = False
-    tof_2.stop_ranging()
-    print()
-    sys.exit(0)
+signal.signal(signal.SIGINT, exit_handler)
 
 while 1:
     if select == 0:
-        signal.signal(signal.SIGINT, exit_handler_1)
-
-        GPIO.output(sensor1_shutdown, GPIO.HIGH)
-        GPIO.output(sensor2_shutdown, GPIO.LOW)
-
-        while running:
-            distance_in_mm = tof_1.get_distance()
-            print("Distance_1: {}mm".format(distance_in_mm))
-            time.sleep(0.1)
-            count += 1
-            if count == limit:
-                count = 0
-                select = 1 
-
-    else:
-        signal.signal(signal.SIGINT, exit_handler_2)
-
         GPIO.output(sensor1_shutdown, GPIO.LOW)
         GPIO.output(sensor2_shutdown, GPIO.HIGH)
 
         while running:
-            distance_in_mm = tof_2.get_distance()
+            distance_in_mm = tof.get_distance()
+             print("Distance_1: {}mm".format(distance_in_mm))
+            time.sleep(0.1)
+            count += 1
+            if count == limit:
+                count = 0
+                select = 1
+                break 
+
+    else:
+        GPIO.output(sensor1_shutdown, GPIO.HIGH)
+        GPIO.output(sensor2_shutdown, GPIO.LOW)
+
+        while running:
+            distance_in_mm = tof.get_distance()
             print("Distance_2: {}mm".format(distance_in_mm))
             time.sleep(0.1)
             count += 1
             if count == limit: 
                 count = 0 
-                select = 0 
+                select = 0
+                break 
