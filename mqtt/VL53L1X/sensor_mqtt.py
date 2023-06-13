7acf8858354c5dce7556a8368e6d321ef681003d
Byeong-gil Jun <junbg@hanyang.ac.kr>
2023-06-13 12:27:06 +0900

    MQTT test files

---
+++ mqtt/VL53L1X/sensor_mqtt.py
@@ -0,0 +1,47 @@
+import time
+import board
+import adafruit_vl53l1x
+import paho.mqtt.client as paho
+
+broker="test.mosquitto.org"
+#port=1883
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published \n")
+    pass
+client1= paho.Client("sensor1")                           #create client object
+client1.on_publish = on_publish                          #assign function to callback
+client1.connect(broker)                                 #establish connection
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+vl53 = adafruit_vl53l1x.VL53L1X(i2c)
+
+# OPTIONAL: can set non-default values
+vl53.distance_mode = 1
+vl53.timing_budget = 100
+
+print("VL53L1X Simple Test.")
+print("--------------------")
+model_id, module_type, mask_rev = vl53.model_info
+print("Model ID: 0x{:0X}".format(model_id))
+print("Module Type: 0x{:0X}".format(module_type))
+print("Mask Revision: 0x{:0X}".format(mask_rev))
+print("Distance Mode: ", end="")
+if vl53.distance_mode == 1:
+    print("SHORT")
+elif vl53.distance_mode == 2:
+    print("LONG")
+else:
+    print("UNKNOWN")
+print("Timing Budget: {}".format(vl53.timing_budget))
+print("--------------------")
+
+vl53.start_ranging()
+
+while True:
+    if vl53.data_ready:
+        print("Distance: {} cm".format(vl53.distance))
+        client1.publish("embed/sensor1",vl53.distance)
+        vl53.clear_interrupt()
+        time.sleep(1.0)
